94b3b3fb792612df3e82c8810abfa438e091190b
Dawid Bajek <dawidbajek03g@gmail.com>
2023-05-31 16:54:22 +0200

    update nav animation

--- js/main.js
+++ js/main.js
@@ -1,14 +1,25 @@
 const nav = document.querySelector('.nav');
 const burgerBtn = document.querySelector('.burger-btn');
 
+const navItems = [...document.querySelectorAll('.nav__item')];
+
+const handlNavItemsAnimation = () => {
+    let delayTime = 0;
+    navItems.forEach(item => {
+        item.classList.toggle("nav-items-animation");
+        item.style.animationDelay=`.${delayTime}s`;
+        delayTime++;
+    })
+}
+
 burgerBtn.addEventListener("click", () => {
     nav.classList.toggle('nav--active');
+    handlNavItemsAnimation();
 })
-
-const navItems = [...document.querySelectorAll('.nav__item')];
 
 navItems.forEach(item=>{
     item.addEventListener("click", ()=>{
         nav.classList.remove('nav--active');
     })
 })
+
