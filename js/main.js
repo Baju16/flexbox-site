a1cec803b6f4f576653a9059dde2d96b3da84fa2
Dawid Bajek <dawidbajek03g@gmail.com>
2023-06-06 13:56:51 +0200

    styling footer and create change nav color script

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
 const nav = document.querySelector('.nav');
 const burgerBtn = document.querySelector('.burger-btn');
-
 const navItems = [...document.querySelectorAll('.nav__item')];
+const navBtnBars = document.querySelector('.burger-btn__bars');
+const allSections = document.querySelectorAll('.section');
+const footerYear = document.querySelector('.footer__year');
 
 const handlNavItemsAnimation = () => {
     let delayTime = 0;
     navItems.forEach(item => {
         item.classList.toggle("nav-items-animation");
+        navBtnBars.classList.remove('black-bars-color');
         item.style.animationDelay=`.${delayTime}s`;
         delayTime++;
     })
@@ -22,4 +25,26 @@ navItems.forEach(item=>{
         nav.classList.remove('nav--active');
     })
 })
+
+const handleCurrentYear = () =>{
+    const year = (new Date).getFullYear();
+    footerYear.innerText = year;
+}
+
+const handleObserver = () => {
+    const currentSection = window.scrollY;
+
+    allSections.forEach(section=>{  
+
+        if (section.classList.contains('white-section') && section.offsetTop <= currentSection + 60) {
+            navBtnBars.classList.add('black-bars-color');
+        }else if (!section.classList.contains('white-section') && section.offsetTop <= currentSection + 60){
+            navBtnBars.classList.remove('black-bars-color');
+        }
+    })
+}
+
+window.addEventListener('scroll', handleObserver);
+
+handleCurrentYear();
 
